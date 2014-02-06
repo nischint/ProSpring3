20738cd4b2fb9ae80b65d4747c2fe0af53b39666
Nischint Ramesh <nischintr@gmail.com>
2014-02-05 21:45:29 -0800

    small edit on defineMeaningOfLife

--- main/java/com/nischint/ch3/BookWormOracle.java
+++ main/java/com/nischint/ch3/BookWormOracle.java
@@ -3,7 +3,7 @@ package com.nischint.ch3;
 public class BookWormOracle implements Oracle {
 	
 	public String defineMeaningOfLife() {
-		return "Encyclopedias are dead, long live Internet!";
+		return "Encyclopedias are dead, you can find everything on Internet!";
 	}
 
 }
